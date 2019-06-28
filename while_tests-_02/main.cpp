277dd0ad5c77a2946c1d01dd55492aa1a115414f
fuqiusen <fu1569996192@qq.com>
2019-06-28 18:49:49 +0800

    while语句测试02 是[1-100]从一到一百+法;

---
+++ while_tests-_02/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+int main(void) {
+
+	int i = 1;
+	int s = 0;
+	while (i <= 100) {
+		s += i;
+		i++;
+
+
+	}
+	cout << "s=" << s << endl;
+
+
+	system("pause");
+	return 0;
+}
